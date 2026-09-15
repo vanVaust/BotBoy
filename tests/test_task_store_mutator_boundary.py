088011f446a4d596678a5be8dace387ef303eb56
vanVaust <vo.smagach@gmail.com>
2026-09-15 20:24:20 +0200

    test: isolate gateway identity in task-store boundary tests

--- tests/test_task_store_mutator_boundary.py
+++ tests/test_task_store_mutator_boundary.py
@@ -3,7 +3,7 @@ from types import SimpleNamespace
 
 from fastapi import HTTPException
 
-from botboy.gateway.app_context import _TaskStoreAuthorizationProxy
+from botboy.gateway.app_context import _TaskStoreAuthorizationProxy, _current_principal, _current_roles
 
 
 class FakeStore:
@@ -22,14 +22,23 @@ class FakeStore:
 
 class TaskStoreMutatorBoundaryTests(unittest.TestCase):
     def setUp(self):
+        self.principal_token = _current_principal.set("")
+        self.roles_token = _current_roles.set(())
         self.store = FakeStore()
         self.proxy = _TaskStoreAuthorizationProxy(self.store, auth_enabled=True)
+
+    def tearDown(self):
+        _current_roles.reset(self.roles_token)
+        _current_principal.reset(self.principal_token)
 
     def test_raw_artifact_mutators_are_blocked(self):
         for name in ("add_artifact", "link_artifacts_from_task"):
             with self.subTest(name=name):
                 with self.assertRaises(HTTPException) as raised:
-                    getattr(self.proxy, name)("task-a")
+                    if name == "link_artifacts_from_task":
+                        getattr(self.proxy, name)("task-a", "task-b")
+                    else:
+                        getattr(self.proxy, name)("task-a")
                 self.assertEqual(raised.exception.status_code, 403)
                 self.assertEqual(self.store.called, [])
 
