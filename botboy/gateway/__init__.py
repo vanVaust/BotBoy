35bf09961f8741989bde7aea0d63f0edee7d64b1
vanVaust <vo.smagach@gmail.com>
2026-09-15 20:39:33 +0200

    security: install fail-closed task-store surface

--- botboy/gateway/__init__.py
+++ botboy/gateway/__init__.py
@@ -8,3 +8,4 @@ from botboy.gateway import recovery_security as _recovery_security  # noqa: F401
 from botboy.gateway import task_summary_security as _task_summary_security  # noqa: F401,E402
 from botboy.gateway import merge_security as _merge_security  # noqa: F401,E402
 from botboy.gateway import store_read_security as _store_read_security  # noqa: F401,E402
+from botboy.gateway import task_store_surface_security as _task_store_surface_security  # noqa: F401,E402
